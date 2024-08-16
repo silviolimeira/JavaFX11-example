35aea35d4558af9492689a6541ae7842e51fb851
Silvio Limeira <sicemal@gmail.com>
2024-08-15 21:47:35 -0300

    add notaS.txt

--- src/main/java/com/almasb/test/FXApp.java
+++ src/main/java/com/almasb/test/FXApp.java
@@ -1,18 +1,80 @@
 package com.almasb.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.util.Pair;
+import javafx.util.StringConverter;
 
 /**
  * @author Almas Baimagambetov (almaslvl@gmail.com)
  */
 public class FXApp extends Application {
+	private final ChoiceBox<Pair<String,String>> assetClass = new ChoiceBox<>();
+
+    private final static Pair<String, String> EMPTY_PAIR = new Pair<>("", "");
+
     @Override
-    public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(new Pane(), 800, 600));
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+
+        Label label = new Label("Asset Class:");
+        assetClass.setPrefWidth(200);
+        Button saveButton = new Button("Save");
+
+        HBox hbox = new HBox(
+                label,
+                assetClass,
+                saveButton);
+        hbox.setSpacing( 10.0d );
+        hbox.setAlignment(Pos.CENTER );
+        hbox.setPadding( new Insets(40) );
+
+        Scene scene = new Scene(hbox);
+
+        initChoice();
+
+        saveButton.setOnAction(
+                (evt) -> System.out.println("saving " + assetClass.getValue())
+        );
+
+        primaryStage.setTitle("ChoicesApp");
+        primaryStage.setScene( scene );
+        primaryStage.show();
+
+    }
+
+    private void initChoice() {
+
+        List<Pair<String,String>> assetClasses = new ArrayList<>();
+        assetClasses.add( new Pair("Equipment", "20000"));
+        assetClasses.add( new Pair("Furniture", "21000"));
+        assetClasses.add( new Pair("Investment", "22000"));
+
+        assetClass.setConverter( new StringConverter<Pair<String,String>>() {
+            @Override
+            public String toString(Pair<String, String> pair) {
+                return pair.getKey();
+            }
+
+            @Override
+            public Pair<String, String> fromString(String string) {
+                return null;
+            }
+        });
+
+        assetClass.getItems().add( EMPTY_PAIR );
+        assetClass.getItems().addAll( assetClasses );
+        assetClass.setValue( EMPTY_PAIR );
+
     }
 
     public static void main(String[] args) {
